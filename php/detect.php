dcc11ae370387ace8e93aa82e12886b77c36701f
Jan Macario <jmacario@gmu.edu>
2022-08-13 21:28:35 -0400

    Improves notification email formatting.

--- php/detect.php
+++ php/detect.php
@@ -10,8 +10,10 @@ add_action( 'activated_plugin', 'gmuj_sci_detect_plugin_activation', 10, 2 );
 function gmuj_sci_detect_plugin_activation($plugin, $network_activation) {
 
     //set email subject and body
-		$email_subject='Mason Site Check-In - Plugin Activated: '.$plugin.' / '.get_bloginfo('name').' ('.$_SERVER['HTTP_HOST'].')';
-		$email_body='Mason Site Check-In - Plugin Activated: '.$plugin.' / '.get_bloginfo('name').' ('.$_SERVER['HTTP_HOST'].')';
+		$email_subject='Mason Site Check-In: Plugin Activated: '.$_SERVER['HTTP_HOST'];
+		$email_body.='Status: Plugin Activated<br />';
+		$email_body.='Website: '.get_bloginfo('name').'('.$_SERVER['HTTP_HOST'].')<br />';
+		$email_body.='Plugin: '.$plugin.'<br />';
 
     //send email
 	    gmuj_sci_send_email($email_subject,$email_body);
@@ -22,8 +24,10 @@ add_action( 'deactivated_plugin', 'gmuj_sci_detect_plugin_deactivation', 10, 2 )
 function gmuj_sci_detect_plugin_deactivation($plugin, $network_activation) {
 
     //set email subject and body
-		$email_subject='Mason Site Check-In - Plugin Deactivated: '.$plugin.' / '.get_bloginfo('name').' ('.$_SERVER['HTTP_HOST'].')';
-		$email_body='Mason Site Check-In - Plugin Deactivated: '.$plugin.' / '.get_bloginfo('name').' ('.$_SERVER['HTTP_HOST'].')';
+		$email_subject='Mason Site Check-In: Plugin Deactivated: '.$_SERVER['HTTP_HOST'];
+		$email_body.='Status: Plugin Deactivated<br />';
+		$email_body.='Website: '.get_bloginfo('name').'('.$_SERVER['HTTP_HOST'].')<br />';
+		$email_body.='Plugin: '.$plugin.'<br />';
     
     //send email
     	gmuj_sci_send_email($email_subject,$email_body);
@@ -35,8 +39,10 @@ add_action( 'switch_theme', 'gmuj_sci_detect_switch_theme', 10, 1 );
 function gmuj_sci_detect_switch_theme($theme) {
 
     //set email subject and body
-		$email_subject='Mason Site Check-In - Theme Switch: '.$theme.' / '.get_bloginfo('name').' ('.$_SERVER['HTTP_HOST'].')';
-		$email_body='Mason Site Check-In - Theme Switch: '.$theme.' / '.get_bloginfo('name').' ('.$_SERVER['HTTP_HOST'].')';
+		$email_subject='Mason Site Check-In: Theme Switched: '.$_SERVER['HTTP_HOST'];
+		$email_body.='Status: Theme Switched<br />';
+		$email_body.='Website: '.get_bloginfo('name').'('.$_SERVER['HTTP_HOST'].')<br />';
+		$email_body.='Theme: '.$theme.'<br />';
     
     //send email
     	gmuj_sci_send_email($email_subject,$email_body);
