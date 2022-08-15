89f5ae45c26a6aac9b50e1fea2d56fdedcc4424b
Jan Macario <jmacario@gmu.edu>
2022-08-14 22:24:14 -0400

    Consolidates all existing 'most recent' REST routes into a single 'most recent modifications' route for simplicity and ensures that this endpoint returns an actual JSON object.

--- php/api.php
+++ php/api.php
@@ -8,54 +8,20 @@ add_action('rest_api_init', 'gmuj_sci_register_routes');
 
 function gmuj_sci_register_routes() {
 
-	// Most recent touch
-	register_rest_route('gmuj-sci', 'most-recent-touch', array(
-			'methods' => 'GET',
-			'callback' => function(){return array('last_modified' => get_most_recent_touch_date());}
-		)
-	);
-	
-	// Most recent post
-	register_rest_route('gmuj-sci', 'most-recent-post', array(
-			'methods' => 'GET',
-			'callback' => 'gmuj_sci_get_most_recent_post_date',
-		)
-	);
-
-	// Most recent page
-	register_rest_route('gmuj-sci', 'most-recent-page', array(
-			'methods' => 'GET',
-			'callback' => 'gmuj_sci_get_most_recent_page_date',
-		)
-	);
-
-	// Most recent attachment
-	register_rest_route('gmuj-sci', 'most-recent-attachment', array(
-			'methods' => 'GET',
-			'callback' => 'gmuj_sci_get_most_recent_attachment_date',
-		)
-	);
-
-	// Most recent user
-	register_rest_route('gmuj-sci', 'most-recent-user', array(
-			'methods' => 'GET',
-			'callback' => 'gmuj_sci_get_most_recent_user_date',
-		)
-	);
-
-	// Most recent login
-	register_rest_route('gmuj-sci', 'most-recent-login', array(
-			'methods' => 'GET',
-			'callback' => 'gmuj_sci_get_most_recent_login_timestamp',
-		)
-	);
-
-	// Most recent login user
-	register_rest_route('gmuj-sci', 'most-recent-login-user', array(
-			'methods' => 'GET',
-			'callback' => 'gmuj_sci_get_most_recent_login_username',
-		)
-	);
-
+	// Most recent modifications
+	register_rest_route('gmuj-sci', 'most-recent-modifications', array(
+		'methods' => 'GET',
+		'callback' => function(){
+			return array(
+				'last_modified' => get_most_recent_touch_date(),
+				'last_modified_post' => gmuj_sci_get_most_recent_post_date(),
+				'last_modified_page' => gmuj_sci_get_most_recent_page_date(),
+				'last_modified_attachment' => gmuj_sci_get_most_recent_attachment_date(),
+				'last_modified_user' => gmuj_sci_get_most_recent_user_date(),
+				'last_login' => gmuj_sci_get_most_recent_login_timestamp(),
+				'last_login_user' => gmuj_sci_get_most_recent_login_username(),
+			);
+		}
+	));
 
 }
