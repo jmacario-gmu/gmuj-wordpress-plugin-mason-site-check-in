4e453ff02c0f2994d879a3436fc7466adb16e973
Jan Macario <jmacario@gmu.edu>
2022-08-17 14:23:28 -0400

    Minor fix to spacing of some notification emails.

--- php/detect.php
+++ php/detect.php
@@ -12,7 +12,7 @@ function gmuj_sci_detect_plugin_activation($plugin, $network_activation) {
     //set email subject and body
 		$email_subject='Mason Site Check-In: Plugin Activated: '.$_SERVER['HTTP_HOST'];
 		$email_body.='Status: Plugin Activated<br />';
-		$email_body.='Website: '.get_bloginfo('name').'('.$_SERVER['HTTP_HOST'].')<br />';
+		$email_body.='Website: '.get_bloginfo('name').' ('.$_SERVER['HTTP_HOST'].')<br />';
 		$email_body.='Plugin: '.$plugin.'<br />';
 
     //send email
@@ -26,7 +26,7 @@ function gmuj_sci_detect_plugin_deactivation($plugin, $network_activation) {
     //set email subject and body
 		$email_subject='Mason Site Check-In: Plugin Deactivated: '.$_SERVER['HTTP_HOST'];
 		$email_body.='Status: Plugin Deactivated<br />';
-		$email_body.='Website: '.get_bloginfo('name').'('.$_SERVER['HTTP_HOST'].')<br />';
+		$email_body.='Website: '.get_bloginfo('name').' ('.$_SERVER['HTTP_HOST'].')<br />';
 		$email_body.='Plugin: '.$plugin.'<br />';
     
     //send email
@@ -41,7 +41,7 @@ function gmuj_sci_detect_switch_theme($theme) {
     //set email subject and body
 		$email_subject='Mason Site Check-In: Theme Switched: '.$_SERVER['HTTP_HOST'];
 		$email_body.='Status: Theme Switched<br />';
-		$email_body.='Website: '.get_bloginfo('name').'('.$_SERVER['HTTP_HOST'].')<br />';
+		$email_body.='Website: '.get_bloginfo('name').' ('.$_SERVER['HTTP_HOST'].')<br />';
 		$email_body.='Theme: '.$theme.'<br />';
     
     //send email
