7e3b5895531ce80181be9d2fc0288f63a2390c3c
Jan Macario <jmacario@gmu.edu>
2025-09-03 11:40:04 -0400

    Modifies the plugin activation and deactivation notification emails to avoid references to the  PHP superglobal, which is not fully populated when requests are made from WP-CLI, resulting in PHP warnings.

--- php/detect.php
+++ php/detect.php
@@ -9,28 +9,39 @@
 add_action( 'activated_plugin', 'gmuj_sci_detect_plugin_activation', 10, 2 );
 function gmuj_sci_detect_plugin_activation($plugin, $network_activation) {
 
-    //set email subject and body
-		$email_subject='Mason Site Check-In: Plugin Activated: '.$_SERVER['HTTP_HOST'];
-		$email_body.='Status: Plugin Activated<br />';
-		$email_body.='Website: '.get_bloginfo('name').' ('.$_SERVER['HTTP_HOST'].')<br />';
-		$email_body.='Plugin: '.$plugin.'<br />';
+	//get site domain
+	$site_domain=preg_replace('/^https?:\/\//i','',site_url());
 
-    //send email
-	    gmuj_sci_send_email($email_subject,$email_body);
+	//set email subject
+	$email_subject='Mason Site Check-In: Plugin Activated: '.$site_domain;
+
+	//set email body
+	$email_body='Status: Plugin Activated<br />';
+	$email_body.='Website: '.get_bloginfo('name').' ('.$site_domain.')<br />';
+	$email_body.='Plugin: '.$plugin.'<br />';
+
+	//send email
+	gmuj_sci_send_email($email_subject,$email_body);
+
 }
 
 //check for plugin deactivation and send notification email
 add_action( 'deactivated_plugin', 'gmuj_sci_detect_plugin_deactivation', 10, 2 );
 function gmuj_sci_detect_plugin_deactivation($plugin, $network_activation) {
 
-    //set email subject and body
-		$email_subject='Mason Site Check-In: Plugin Deactivated: '.$_SERVER['HTTP_HOST'];
-		$email_body.='Status: Plugin Deactivated<br />';
-		$email_body.='Website: '.get_bloginfo('name').' ('.$_SERVER['HTTP_HOST'].')<br />';
-		$email_body.='Plugin: '.$plugin.'<br />';
-    
-    //send email
-    	gmuj_sci_send_email($email_subject,$email_body);
+	//get site domain
+	$site_domain=preg_replace('/^https?:\/\//i','',site_url());
+
+	//set email subject
+	$email_subject='Mason Site Check-In: Plugin Deactivated: '.$site_domain;
+
+	//set email body
+	$email_body='Status: Plugin Deactivated<br />';
+	$email_body.='Website: '.get_bloginfo('name').' ('.$site_domain.')<br />';
+	$email_body.='Plugin: '.$plugin.'<br />';
+
+	//send email
+	gmuj_sci_send_email($email_subject,$email_body);
 
 }
 
